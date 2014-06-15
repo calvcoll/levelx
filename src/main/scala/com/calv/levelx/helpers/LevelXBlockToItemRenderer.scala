642793be1364bac5ffdae670eca10dffc412d69f
Calv Collins <calvcoll@gmail.com>
2014-06-15 19:34:41 +0100

    Fixed the Y-Positioning of models in the Block -> Item Renderer.

--- src/main/scala/com/calv/levelx/helpers/LevelXBlockToItemRenderer.scala
+++ src/main/scala/com/calv/levelx/helpers/LevelXBlockToItemRenderer.scala
@@ -3,13 +3,17 @@ package com.calv.levelx.helpers
 import net.minecraftforge.client.IItemRenderer
 import net.minecraft.item.ItemStack
 import net.minecraftforge.client.IItemRenderer.{ItemRendererHelper, ItemRenderType}
+import org.lwjgl.opengl.GL11
 
 /**
  * Created by Calv on 15/06/14.
  */
 class LevelXBlockToItemRenderer(tileEntityRenderer :LevelXTileEntityRenderer) extends IItemRenderer {
   override def renderItem(renderType :ItemRenderType, item :ItemStack, data :AnyRef*) = {
+    GL11.glPushMatrix()
+    GL11.glTranslatef(0F,-0.15F,0F)
     tileEntityRenderer.renderTileEntity()
+    GL11.glPopMatrix()
   }
 
   override def shouldUseRenderHelper(`type`: ItemRenderType, item: ItemStack, helper: ItemRendererHelper): Boolean = true
